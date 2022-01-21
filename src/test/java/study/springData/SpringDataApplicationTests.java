f750d96f03359c7cf1309a81938e598244010a4b
Jheeseong <gmltjd6801@gmail.com>
2022-01-21 16:25:41 +0900

    Initial commit

---
+++ src/test/java/study/springData/SpringDataApplicationTests.java
@@ -0,0 +1,15 @@
+package study.springData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
